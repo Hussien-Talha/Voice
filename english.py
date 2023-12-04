82d30413b590954238956e166541effd32d08406
Sonson296 <Hussien.a.m.talha@gmail.com>
2023-12-04 15:01:33 +0200

    added arabic

---
+++ english.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+
+def listen():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say something:")
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said:", text)
+        with open("text.txt", "w", encoding="utf-8") as file:
+            file.write(f"You said: {text}")
+        print("Speech recognition result saved to text.txt.")
+    except sr.UnknownValueError:
+        print("Sorry, couldn't understand audio.")
+    except sr.RequestError as e:
+        print(f"Error connecting to Google Speech Recognition service: {e}")
+
+listen()
